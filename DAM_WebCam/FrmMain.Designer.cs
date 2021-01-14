a63be1857fb444320278529caa7bccf29a41a095
Alvaro Rodriguez <a_rsc@hotmail.com>
2021-01-14 23:12:50 +0100

    first commit

---
+++ DAM_WebCam/FrmMain.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace DAM_WebCam
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.BtCam = new System.Windows.Forms.Button();
+            this.ImgCam = new Emgu.CV.UI.ImageBox();
+            this.TmCam = new System.Windows.Forms.Timer(this.components);
+            this.TbTextQR = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ImgCam)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtCam
+            // 
+            this.BtCam.BackColor = System.Drawing.Color.YellowGreen;
+            this.BtCam.Location = new System.Drawing.Point(12, 387);
+            this.BtCam.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.BtCam.Name = "BtCam";
+            this.BtCam.Size = new System.Drawing.Size(459, 33);
+            this.BtCam.TabIndex = 4;
+            this.BtCam.Text = "Iniciar càmera";
+            this.BtCam.UseVisualStyleBackColor = false;
+            this.BtCam.Click += new System.EventHandler(this.btCam_Click);
+            // 
+            // ImgCam
+            // 
+            this.ImgCam.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ImgCam.Location = new System.Drawing.Point(12, 12);
+            this.ImgCam.Name = "ImgCam";
+            this.ImgCam.Size = new System.Drawing.Size(459, 341);
+            this.ImgCam.TabIndex = 2;
+            this.ImgCam.TabStop = false;
+            // 
+            // TmCam
+            // 
+            this.TmCam.Tick += new System.EventHandler(this.TmCam_Tick);
+            // 
+            // TbTextQR
+            // 
+            this.TbTextQR.Enabled = false;
+            this.TbTextQR.Location = new System.Drawing.Point(12, 359);
+            this.TbTextQR.Name = "TbTextQR";
+            this.TbTextQR.Size = new System.Drawing.Size(459, 22);
+            this.TbTextQR.TabIndex = 5;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 431);
+            this.Controls.Add(this.TbTextQR);
+            this.Controls.Add(this.BtCam);
+            this.Controls.Add(this.ImgCam);
+            this.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "FrmMain";
+            this.Text = "WebCam ";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmMain_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.ImgCam)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtCam;
+        private Emgu.CV.UI.ImageBox ImgCam;
+        private System.Windows.Forms.Timer TmCam;
+        private System.Windows.Forms.TextBox TbTextQR;
+    }
+}
+
---
+++ DAM_WebCam/FrmMain.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+/*
+    Aquest projecte necessita la llibreria EmguCV que és una llibreria per a treballar amb OpenCV dins de l'entorn C# de Visual Studio
+    // http://www.emgu.com/wiki/index.php/Main_Page
+
+    Hem d'afegir les referències necessàries a les DLL d'EmguCV que contenen les eines que necessitem
+
+    Al quadre d'eines de Visual Studio hem de fer botó dret i seleccionar <Escollir elements>. Després anem, amb el botó <Examinar> a la carpeta on tenim les llibreries d'EmguCV (carpeta bin) i seleccionem Emgu.CV.UI.dll. 
+    En els components del quadre d'eines ens n'apareixeran de nous, farem servir ImageBox
+
+    IMAGEBOX - eina d'Emgu Corporation
+*/
+
+// Llibreries d'EmguCV: ..\bin\DEBUG
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+// Llibreria per llegir QR code
+using ZXing;
+// Encode: create QR code image
+// Decode: read an image containing one or more QR codes.
+
+namespace DAM_WebCam
+{
+    public partial class FrmMain : Form
+    {
+        // Càmera
+        private Capture kam; // Capture és una captura d'imatges o videos des de qualsevol fitxer de càmera o video: class Emgu.CV.Capture
+        private string textQR = "";
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void TmCam_Tick(object sender, EventArgs e)
+        {
+            GestioFotograma(sender, e);
+        }
+
+        private void GestioFotograma(object sender, EventArgs arg)
+        {
+            Mat frame; // Mat és un tipus matriu definit en l'estàndard OpenCV que s'associa a les imatges: class Emgu.CV.Mat
+            BarcodeReader lector = new BarcodeReader { AutoRotate=true }; // BarcodeReader és un tipus definit a la referencia zxing
+
+            try
+            {
+                frame = kam.QueryFrame(); // Captura del frame actual segons el timer TmCam
+                ImgCam.Image = frame; // ImgCam és una eina (herramienta del formulari) allotjada a la llibreria Emgu.CV.UI.ImageBox
+
+                // Examinador de objetos Emgu.CV.UI.Image
+                // ImgCam.Image.Save(string filename); // https://docs.microsoft.com/es-es/dotnet/api/system.drawing.image.save?view=dotnet-plat-ext-5.0#System_Drawing_Image_Save_System_String_
+
+                if(ImgCam.Image != null && lector.Decode(ImgCam.Image.Bitmap) != null)
+                {
+                    TbTextQR.Text=lector.Decode(ImgCam.Image.Bitmap).Text;
+
+                    if(!TbTextQR.Text.Equals(TextQR))
+                    {
+                        System.Diagnostics.Process.Start(TbTextQR.Text);
+                        TextQR=TbTextQR.Text;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Excepció", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btCam_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                kam = new Capture(0); // Inicialitzem la càmara, el 0 és l'índex per si tenim més d'una càmera (Administrador de dispositius)
+                BtCam.Visible = false;
+                TmCam.Start();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Excepció", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void FrmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(kam != null)
+            {
+                kam.Stop();
+                kam.Dispose();
+            }
+        }
+
+        public string TextQR { get => textQR; set => textQR=value; }
+    }
+}
